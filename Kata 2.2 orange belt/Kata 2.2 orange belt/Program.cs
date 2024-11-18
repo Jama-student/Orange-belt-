8124c7c1a1d5427fff497990e4985676d59eadab
Jama-student <jamafahad097@gmail.com>
2024-11-18 22:34:28 +0100

    Add files via upload

---
+++ Kata 2.2 orange belt/Kata 2.2 orange belt/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Kata_2._2_orange_belt;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create two characters
+        Character warrior = new Character("Warrior", 100);
+        Character goblin = new Character("Goblin", 50);
+
+        // Subscribe to the HealthChanged event
+        warrior.HealthChanged += (sender, e) => Console.WriteLine($"{warrior.Name}'s health is now {warrior.Health}");
+        goblin.HealthChanged += (sender, e) => Console.WriteLine($"{goblin.Name}'s health is now {goblin.Health}");
+
+        // Perform an attack
+        warrior.Attack(goblin);  // Warrior attacks Goblin
+
+        // Output: Warrior attacks Goblin and Goblin's health should be updated
+    }
+}
+
+class Character
+{
+    public string Name { get; set; }
+    public int Health { get; set; }
+
+    // Declare a delegate to represent character actions
+    public delegate void CharacterAction();
+
+    // Declare an event to notify when health changes
+    public event EventHandler HealthChanged;
+
+    public Character(string name, int health)
+    {
+        Name = name;
+        Health = health;
+    }
+
+    // Attack method to reduce health of a target character
+    public void Attack(Character target)
+    {
+        Console.WriteLine($"{Name} attacks {target.Name}");
+
+        // Reduce target's health
+        target.Health -= 20;
+
+        // Trigger the HealthChanged event
+        OnHealthChanged(target);
+    }
+
+    // Method to trigger the HealthChanged event
+    protected virtual void OnHealthChanged(Character target)
+    {
+        // If there are subscribers, trigger the event
+        HealthChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
