54dfa66a74d5f9fcc11877d741d590d723fadef3
Jama-student <jamafahad097@gmail.com>
2024-11-20 10:42:09 +0100

    Update Program.cs

--- Level 1 Striped Yellow Belt/Level 1 Striped Yellow Belt/Program.cs
+++ Level 1 Striped Yellow Belt/Level 1 Striped Yellow Belt/Program.cs
@@ -6,29 +6,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a Warrior and a Healer with health values
+        
         Character warrior = new Character("Warrior", 40);
         Character healer = new Character("Healer", 60);
 
-        // Assign Actions to characters using lambdas
+        
         warrior.PrimaryAction = () => Console.WriteLine("Warrior attacks the enemy!");
         healer.PrimaryAction = () => Console.WriteLine("Healer casts Heal!");
 
-        // Check health and invoke the appropriate action
-        // Warrior attacks if health is below 50
+        
+
         if (warrior.Health < 50)
         {
             warrior.PrimaryAction.Invoke();
         }
 
-        // Healer heals if health is below 50 or prioritizes healing another character
+        
         if (healer.Health < 50)
         {
             healer.PrimaryAction.Invoke();
         }
         else
         {
-            // Healer will heal the warrior since his health is below 50
+            
             healer.PrimaryAction = () => Console.WriteLine("Healer heals Warrior!");
             healer.PrimaryAction.Invoke();
         }
